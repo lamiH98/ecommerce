b222c571b1cf92ff550c043ea70f6e911623ad34
lamiH98 <52789739+lamiH98@users.noreply.github.com>
2021-04-12 12:03:14 +0300

    Update SliderController.php

--- app/Http/Controllers/dashboard/SliderController.php
+++ app/Http/Controllers/dashboard/SliderController.php
@@ -36,7 +36,7 @@ class SliderController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(SliderRequest $request)
+    public function store(Request $request)
     {
         if($request->hasFile('slider_image')) {
             $uploadImage = $request->file('slider_image');
