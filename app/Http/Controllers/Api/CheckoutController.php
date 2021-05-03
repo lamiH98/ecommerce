741ceb1fe03cbdc232c5c1610ddaf8d83aa89c7e
lamiH98 <52789739+lamiH98@users.noreply.github.com>
2021-05-04 00:40:03 +0300

    Update CheckoutController.php

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -23,7 +23,7 @@ class CheckoutController extends Controller
      */
     public function store(Request $request)
     {
-           dd($request->all());
+        return $this->sendResponse('brands', $request->all(), 'All Brand');
         // Check race condition when there are less item available to purchase
 //         if($this->productAreNotLongerAvailable($request)) {
 //             return $this->sendError('product Are Not Longer Available');
